13aae2c3090a0c610d9a4cf85e939b8675f6ba64
David Lejeune <Lejeune.David@outlook.com>
2016-12-09 21:39:27 +0100

    a little bit of comment

--- lib/url_parser.rb
+++ lib/url_parser.rb
@@ -6,23 +6,33 @@ class UrlParser
 
 private
 attr_accessor :url
+
+
 public
+
 def initialize url
   @url = url
 end
 
 def host
+  #  using dry method split
   (split "://").last
 end
 
 def scheme
+  # before the dry method split
   # parts = url.split "://"
   # parts[0]
+
+  #  this would be too 'easy' :/
   # URI.parse(url).scheme
+
+  #  using dry method split
     (split "://").first
 end
 
 def path
+    # this doesn't correctly display a path consiting of multiples slashes
     # (host.split "/").last
 
     # http://stackoverflow.com/questions/5129574/ruby-regexp-capture-the-path-of-url
